d7bdc51745dd381cb0c4fc0847ca8d0f91bb1994
romanlum <romanlum>
2015-05-29 21:56:30 +0200

    [practice 05] finished

--- SWO4/uebungMoodle5/Beispiel/src/at/lumetsnet/swe4/collections/SortedTreeSet.java
+++ SWO4/uebungMoodle5/Beispiel/src/at/lumetsnet/swe4/collections/SortedTreeSet.java
@@ -2,7 +2,8 @@ package at.lumetsnet.swe4.collections;
 
 public interface SortedTreeSet<T> extends SortedSet<T> {
 	/***
-	 * Gets the height of the tree height starts with 0, this means that a tree
+	 * Gets the height of the tree 
+	 * height starts with 0, this means that a tree
 	 * with only one item has height 0
 	 * 
 	 * @return
